6ccf91c38a7b9b52e27e512752b628e0e5de68c9
Stephen Roantree <stroantree@gmail.com>
2014-03-08 19:40:35 +0000

    Follow character script.

---
+++ Assets/CameraFollowCharacterBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowCharacterBehavior : MonoBehaviour {
+
+	public GameObject _Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3 (_Player.transform.position.x, _Player.transform.position.y, this.transform.position.z);
+	}
+}
